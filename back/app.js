d826b83438c420e2e7d625176d00047aa3b5e00e
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2021-11-10 17:12:04 +0900

    백엔드 노드 서버 구축하기: 익스프레스로 라우팅하기

--- back/app.js
+++ back/app.js
@@ -1,15 +1,45 @@
 // node는 서버가 아니다.
-// node가 제공해주는 http 모듈
-const http = require('http')
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method)
-  res.write('<h1>Hello node1</h1>')
-  res.write('<h2>Hello node2</h2>')
-  res.write('<h3>Hello node3</h3>')
-  res.write('<h4>Hello node4</h4>')
-  res.end('Hello node5')
+// node가 제공해주는 http 모듈이 서버의 역할을 해주는 것(node가 해주는게 아니다)
+const express = require('express')
+
+const app = express()
+
+// app.get: 가져오다
+// app.post: 생성하다
+// app.put: 전체수정
+// app.delete: 제거
+// app.patch: 부분수정
+// app.options: 찔러보기(나 요청보낼 수 있어..?)
+// app.head: 헤더만 가져오기(헤더/바디)
+app.get('/', (req, res) => {
+  res.send('hello express')
 })
-// http가 서버의 역할을 해주는 것(node가 해주는게 아니다)
-server.listen(3065, () => {
+
+app.get('/api', (req, res) => {
+  res.send('hello api')
+})
+
+app.get('/api/posts', (req, res) => {
+  // data는 보통 json으로 표현한다.
+  // 실제로 api들은 json을 응답한다.
+  res.json([
+    { id: 1, content: 'hello1' },
+    { id: 2, content: 'hello2' },
+    { id: 3, content: 'hello3' },
+  ])
+})
+
+app.post('/api/posts', (req, res) => {
+  res.json({
+    id: 1,
+    content: 'hello'
+  })
+})
+
+app.delete('/api/posts', (req, res) => {
+  res.json({ id: 1 })
+})
+
+app.listen(3065, () => {
   console.log('서버 실행 중')
 })
