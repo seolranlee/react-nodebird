55577b1e7c287cd63c0e8112de1fe0c56b2765e6
seolranlee <seolranlee@seolranleeui-MacBookPro.local>
2022-01-04 18:30:52 +0900

    middlewares 오탈자수정

--- back/routes/middlewares.js
+++ back/routes/middlewares.js
@@ -1,4 +1,4 @@
-exports.isLoggedIn = (req, res, nex) => {
+exports.isLoggedIn = (req, res, next) => {
   // req.isAuthenticated(): passport에서 로그인 여부를 제공해줌.
   if (req.isAuthenticated()) {
     next()
@@ -7,7 +7,7 @@ exports.isLoggedIn = (req, res, nex) => {
   }
 }
 
-exports.isNotLoggedIn = (req, res, nex) => {
+exports.isNotLoggedIn = (req, res, next) => {
   // req.isAuthenticated(): passport에서 로그인 여부를 제공해줌.
   if (!req.isAuthenticated()) {
     // next에 인자가 아무것도 없으면 다음 미들 웨어로 간다. 미들웨어: (req, res, next) => {} 함수.
